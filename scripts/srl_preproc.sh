b003490023ede7767e6eb21a55d03574bca7683d
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-10-17 13:55:22 +0100

    refine preproc

--- scripts/srl_preproc.sh
+++ scripts/srl_preproc.sh
@@ -5,10 +5,11 @@ python -mnnet.run.srl.conll --data data/conll2009/CoNLL2009-ST-evaluation-Englis
 python -mnnet.run.srl.conll --data data/conll2009/CoNLL2009-ST-English-train.txt  > data/conll2009/train_conll2009.json.dep
 python -mnnet.run.srl.conll --data data/conll2009/CoNLL2009-ST-evaluation-English-ood.txt > data/conll2009/ood_conll2009.json.dep
 
-python -mnnet.run.srl.training_sample --data data/conll2009/train_conll2009.json.dep --frames data/nombank_descriptions-1.0+prop3.1.json > conll2009.train.dep_l
 python -mnnet.run.srl.training_sample --data data/conll2009/ood_conll2009.json.dep --frames data/nombank_descriptions-1.0+prop3.1.json > conll2009.ood.dep_l
 python -mnnet.run.srl.training_sample --data data/conll2009/test_conll2009.json.dep --frames data/nombank_descriptions-1.0+prop3.1.json > conll2009.test.dep_l
 python -mnnet.run.srl.training_sample --data data/conll2009/dev_conll2009.json.dep --frames data/nombank_descriptions-1.0+prop3.1.json > conll2009.dev.dep_l
+python -mnnet.run.srl.training_sample --data data/conll2009/train_conll2009.json.dep --frames data/nombank_descriptions-1.0+prop3.1.json > conll2009.train.dep_l
+
 
 #cat conll2009.ood conll2009.test conll2009.train conll2009.dev conll2009.test > conll2009.combined
 ##cut -f6 conll2009.combiccned | python -mnnet.ml.voc --tokenizer space >frames.voc.conll2009
