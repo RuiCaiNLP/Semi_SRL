f28f7b3bf53ccf2e38f4c987b7d8e39101fccb90
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-10-20 12:06:08 +0100

    New Dep

--- nnet/nn_models/Different_Proj.py
+++ nnet/nn_models/Different_Proj.py
@@ -264,7 +264,7 @@ class BiLSTMTagger(nn.Module):
 
 
 
-        Predicate_identification = self.Idenficiation(F.relu(self.MLP_identification(Word_hidden)))
+        Predicate_identification = self.Idenficiation(F.relu(self.MLP_identification(hidden_states_0, hidden_states_1)))
         Predicate_identification_space = Predicate_identification.view(
             len(sentence[0]) * self.batch_size, -1)
 
