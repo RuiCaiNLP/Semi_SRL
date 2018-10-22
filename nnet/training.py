1d7fd00bad3beb32d6c46246d74e07dd5bf0d478
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-10-22 12:12:33 +0100

    New Dep

--- nnet/training.py
+++ nnet/training.py
@@ -140,10 +140,9 @@ def train(model, train_set, dev_set, test_set, epochs, converter, dbg_print_rate
             #else:
             #    Final_loss = SRLloss
 
-            if batch_idx % 5 == 0:
-                Final_loss = SRLloss + DEPloss + SPEDEPloss
-            else:
-                Final_loss = SRLloss + DEPloss
+
+            Final_loss = SRLloss + DEPloss + SPEDEPloss
+
             Final_loss.backward()
             #clip_grad_norm_(parameters=model.hidden2tag_M.parameters(), max_norm=norm)
             #clip_grad_norm_(parameters=model.hidden2tag_H.parameters(), max_norm=norm)
