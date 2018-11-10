08f8bbb61afbef69823aa96af56500ed9f855188
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-11-10 16:32:45 +0000

    Biaffine.py

--- scripts/training.sh
+++ scripts/training.sh
@@ -22,7 +22,7 @@ python -mnnet.run.srl.run \
 --epochs 30 \
 --out conll2009_rm0_pl_a.25_sskip_h512_d.0_l4 \
 --params-path Less_annotation_512_0.9.pkl \
---hps "{'id': 1, 'sent_edim': 100, 'sent_hdim': 512, \
+--hps "{'id': 1, 'sent_edim': 100, 'sent_hdim': 400, \
 'frame_edim': 128, 'role_edim': 128, 'pos_edim': 16, 'rec_layers': 1, 'gc_layers': 0, \
 'pos': True, 'rm':0, 'alpha': 0.25, \
 'p_lemmas':True}"
