06abcb52eaa004e55990e4146adc0d2aeb00c986
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-11-14 16:49:51 +0000

    New Dep

--- nnet/nn_models/SRL_DEP_STACK.py
+++ nnet/nn_models/SRL_DEP_STACK.py
@@ -141,10 +141,10 @@ class BiLSTMTagger(nn.Module):
         self.BiLSTM_DEP = nn.LSTM(input_size=sent_embedding_dim_SRL, hidden_size=lstm_hidden_dim, batch_first=True,
                                   bidirectional=True, num_layers=self.num_layers)
 
-        init.orthogonal_(self.BiLSTM_SRL.all_weights[0][0])
-        init.orthogonal_(self.BiLSTM_SRL.all_weights[0][1])
-        init.orthogonal_(self.BiLSTM_SRL.all_weights[1][0])
-        init.orthogonal_(self.BiLSTM_SRL.all_weights[1][1])
+        init.orthogonal_(self.BiLSTM_DEP.all_weights[0][0])
+        init.orthogonal_(self.BiLSTM_DEP.all_weights[0][1])
+        init.orthogonal_(self.BiLSTM_DEP.all_weights[1][0])
+        init.orthogonal_(self.BiLSTM_DEP.all_weights[1][1])
 
         self.Non_Predicate_Proj_DEP = nn.Linear(2 * lstm_hidden_dim, lstm_hidden_dim)
         self.Predicate_Proj_DEP = nn.Linear(2 * lstm_hidden_dim, lstm_hidden_dim)
