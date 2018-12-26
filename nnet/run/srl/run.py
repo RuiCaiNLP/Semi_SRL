6b17ffdfd2f258bc7b581b3ad6e963dec35f726c
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2018-12-26 13:48:23 +0000

    SRL_only

--- nnet/run/srl/run.py
+++ nnet/run/srl/run.py
@@ -13,6 +13,7 @@ def bio_reader(record):
     labels_voc = labels_voc.split(' ')
 
     labels_voc.insert(0, '<pad>')
+    log(labels_voc)
     frame = [frame] * len(labels_voc)
     words = []
     for word in sent.split(' '):
