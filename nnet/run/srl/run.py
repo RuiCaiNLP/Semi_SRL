d60cde1c827adbab8b3bc69b50c93846b5e95124
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-01-23 17:11:12 +0000

    New Dep

--- nnet/run/srl/run.py
+++ nnet/run/srl/run.py
@@ -1,7 +1,7 @@
 from nnet.run.runner import *
 from nnet.ml.voc import *
 from functools import partial
-from nnet.nn_models.base_learner.CVT_POS import BiLSTMTagger
+from nnet.nn_models.base_learner.CVT_POS_DEP import BiLSTMTagger
 
 
 all_labels_voc = []
