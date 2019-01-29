fc41337396b1458f46cafb33c6cee0675e98965c
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-01-29 19:14:51 +0000

    SRL

--- nnet/nn_models/base_learner/SRL_CVT.py
+++ nnet/nn_models/base_learner/SRL_CVT.py
@@ -273,7 +273,7 @@ class BiLSTMTagger(nn.Module):
 
 
         TagProbs_use_softmax = F.softmax(TagProbs_use, dim=2).detach()
-        Entroy_Weights = -torch.sum(torch.log2(TagProbs_use_softmax) * TagProbs_use_softmax, dim=2).detach()
+        Entroy_Weights = 1-torch.sum(torch.log2(TagProbs_use_softmax) * TagProbs_use_softmax, dim=2).detach()
 
         sample_nums = lengths.sum()
         unlabeled_loss_function = nn.KLDivLoss(reduce=False)
