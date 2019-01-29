8c844c53685731ed6f20ff1dc7c826f444943047
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-01-29 18:21:19 +0000

    not to shuffle everything

--- nnet/nn_models/base_learner/SRL_CVT.py
+++ nnet/nn_models/base_learner/SRL_CVT.py
@@ -273,7 +273,9 @@ class BiLSTMTagger(nn.Module):
 
 
         TagProbs_use_softmax = F.softmax(TagProbs_use, dim=2).detach()
-        Entroy_Weights = 1 + torch.sum(torch.log2(TagProbs_use_softmax) * TagProbs_use_softmax, dim=2)
+        Entroy_Weights = 1 + torch.sum(torch.log2(TagProbs_use_softmax) * TagProbs_use_softmax, dim=2).detach()
+        log(Entroy_Weights)
+
         sample_nums = lengths.sum()
         unlabeled_loss_function = nn.KLDivLoss(reduce=False)
 
