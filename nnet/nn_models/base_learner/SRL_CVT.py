093e39fedd2313ce5f9b3ce58bc190870cbbdce6
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-02-07 09:37:41 +0000

    all random

--- nnet/nn_models/base_learner/SRL_CVT.py
+++ nnet/nn_models/base_learner/SRL_CVT.py
@@ -450,7 +450,7 @@ class BiLSTMTagger(nn.Module):
                 index_set.append(j)
                 if j >= lengths[i]:
                     break
-                if Predicate_probs[i][j][1] > Predicate_probs[i][j][0]:
+                if Predicate_probs[i][j][1] > Predicate_probs[i][j][0] and False:
                     candidate_set.append(j)
             if len(candidate_set) > 0:
                 index = random.sample(candidate_set, 1)
