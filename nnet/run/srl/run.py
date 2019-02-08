71d30961714dddb9371c3d9ca282149f09090dd9
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-02-08 16:07:12 +0000

    PI_2

--- nnet/run/srl/run.py
+++ nnet/run/srl/run.py
@@ -1,7 +1,7 @@
 from nnet.run.runner import *
 from nnet.ml.voc import *
 from functools import partial
-from nnet.nn_models.base_learner.PI_1 import BiLSTMTagger
+from nnet.nn_models.base_learner.PI_2 import BiLSTMTagger
 
 
 all_labels_voc = []
